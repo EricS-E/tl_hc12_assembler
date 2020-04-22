6a61ff4a2894f2d101bbe35be443cf3573078995
eren <eren>
2020-04-22 16:24:35 -0500

    Add formatting

--- assembler.js
+++ assembler.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-let fs = require("fs");
+let fs = require('fs');
 
-let result = "";
+let result = '';
 let pc = 0;
 let tags = [];
 
-const instructions = require("./instructions");
+const instructions = require('./instructions');
 
 const addressingModes = instructions.addressingModes;
 const directives = instructions.directives;
@@ -15,68 +15,69 @@ const findAddressingMode = instructions.findAddressingMode;
 const instructionSet = instructions.set;
 const parseNumber = instructions.parseNumber;
 
-let addressingMode = instructionTokens => {
+let addressingMode = (instructionTokens) => {
   const [instructionName, ...operands] = instructionTokens;
   let instructionInfo = findInstruction(instructionName);
   let mode = null;
-  if(instructionInfo.modes.length == 1)
-    return instructionInfo.modes[0];
-  instructionInfo.modes.forEach(currentMode => {
+  if (instructionInfo.modes.length == 1) return instructionInfo.modes[0];
+  instructionInfo.modes.forEach((currentMode) => {
     let modeInfo = findAddressingMode(currentMode.type);
     if (modeInfo && modeInfo.testFunction(operands, pc)) {
       mode = currentMode;
     }
   });
   if (mode) return mode;
-  else return { type: "err" };
+  else return { type: 'err' };
 };
 
 function preprocessString(data) {
-  let processedData = data.replace(/\t/g, " ");
-  processedData = processedData.replace(/ +/g, " ");
-  processedData = processedData.replace(/;.*\n/g, "\n");
+  let processedData = data.replace(/\t/g, ' ');
+  processedData = processedData.replace(/ +/g, ' ');
+  processedData = processedData.replace(/;.*\n/g, '\n');
   processedData = processedData.toLowerCase();
   return processedData;
 }
 
 function processDirective(instructionTokens) {
   const [directiveName, ...operands] = instructionTokens;
-  if (directiveName == "org") {
+  if (directiveName == 'org') {
     pc = parseNumber(operands[0]);
-    result += parseNumber(operands[0]).toString(16).padStart(4, '0') + "\n";
+    result += parseNumber(operands[0]).toString(16).padStart(4, '0') + '\n';
   }
-  if (directiveName == "bsz") {
-    result += pc.toString(16) + " ";
-    for (let i = 0; i < parseNumber(operands[0]); i++) result += "00 ";
-    result += "\n";
+  if (directiveName == 'bsz') {
+    result += pc.toString(16) + ' ';
+    for (let i = 0; i < parseNumber(operands[0]); i++) result += '00 ';
+    result += '\n';
     pc += parseNumber(operands[0]);
   }
-  if (directiveName == "fcb") {
-    result += pc.toString(16) + " ";
-    operands.forEach(element => {
-      result += parseInt(element).toString(16) + " ";
+  if (directiveName == 'fcb') {
+    result += pc.toString(16) + ' ';
+    operands.forEach((element) => {
+      result += parseInt(element).toString(16) + ' ';
     });
-    result += "\n";
+    result += '\n';
     pc += operands.length;
   }
-  if (directiveName == "fcc") {
-    result += pc.toString(16) + " ";
+  if (directiveName == 'fcc') {
+    result += pc.toString(16) + ' ';
     for (let i = 1; i < operands[0].length - 1; i++) {
-      result += operands[0].charCodeAt(i) + " ";
+      result += operands[0].charCodeAt(i) + ' ';
     }
-    result += "\n";
+    result += '\n';
     pc += operands[0].length - 2;
   }
-  if (directiveName == "equ") {
+  if (directiveName == 'equ') {
     tags[tags.length - 1].address = parseNumber(operands[0]);
   }
-  if (directiveName == "dc.b" ) {
-    result += pc.toString(16) + " ";
-    dirOperands = operands[0].split(',').map(e => parseInt(e).toString(16).padStart(2, '0'));
-    dirOperands.forEach(element => {
-      result += element + " ";
+  if (directiveName == 'dc.b') {
+    result += pc.toString(16) + ' ';
+    dirOperands = operands[0]
+      .split(',')
+      .map((e) => parseInt(e).toString(16).padStart(2, '0'));
+    dirOperands.forEach((element) => {
+      result += element + ' ';
     });
-    result += "\n";
+    result += '\n';
     pc += dirOperands.length;
   }
 }
@@ -87,10 +88,10 @@ let analyseInstruction = (instructionTokens, _index) => {
     if (!!findInstruction(instructionName)) {
       result += `\tInstruction: ${instructionName}\n`;
       let mode = addressingMode(instructionTokens);
-      if (mode.type != "err") {
+      if (mode.type != 'err') {
         instructionSize =
           Math.ceil(mode.opcode.length / 2) +
-          addressingModes.find(amode => {
+          addressingModes.find((amode) => {
             return amode.type == mode.type;
           }).length;
         result += `\tMode: ${mode.type}\n`;
@@ -100,7 +101,7 @@ let analyseInstruction = (instructionTokens, _index) => {
         result += `${pc.toString(16)} ${mode.opcode} ${parsedOperands}\n`;
         pc += instructionSize;
       } else {
-        result += "\tInvalid addressing mode or range\n";
+        result += '\tInvalid addressing mode or range\n';
       }
     } else if (!!findDirective(instructionTokens[0]))
       processDirective(instructionTokens);
@@ -109,42 +110,48 @@ let analyseInstruction = (instructionTokens, _index) => {
 };
 
 let analyseLine = (line, _index) => {
-  if (line != "") {
-    let tokens = line.split(" ");
-    if (tokens[0] != "") {
+  if (line != '') {
+    let tokens = line.split(' ');
+    if (tokens[0] != '') {
       tags.push({ tag: tokens[0], address: pc });
     }
     analyseInstruction(tokens.slice(1));
   }
-  result += "\n";
+  result += '\n';
 };
 
 if (process.argv.length < 3) {
-  console.log("Usage: assembler [FILE]");
+  console.log('Usage: assembler [FILE]');
   process.exit(1);
 }
 let filename = process.argv[2];
 
-fs.readFile(filename, "utf8", (error, rawData) => {
+fs.readFile(filename, 'utf8', (error, rawData) => {
   if (error) console.log(error);
   else {
     let data = preprocessString(rawData);
-    let instructions = data.split("\n");
+    let instructions = data.split('\n');
     instructions.forEach((line, lineIndex) => {
       analyseLine(line, lineIndex);
     });
-    console.log('TABSIM:\n')
-    tags.forEach(tag => {
+    console.log('TABSIM:\n');
+    tags.forEach((tag) => {
       console.log(tag.tag, tag.address.toString(16));
-      data = data.replace(new RegExp(` ${tag.tag}`, 'g'), ' $' + tag.address.toString(16));
-      data = data.replace(new RegExp(`,${tag.tag}`, 'g'), ',$' + tag.address.toString(16));
+      data = data.replace(
+        new RegExp(` ${tag.tag}`, 'g'),
+        ' $' + tag.address.toString(16)
+      );
+      data = data.replace(
+        new RegExp(`,${tag.tag}`, 'g'),
+        ',$' + tag.address.toString(16)
+      );
     });
-    instructions = data.split("\n");
-    result = "";
+    instructions = data.split('\n');
+    result = '';
     instructions.forEach((line, lineIndex) => {
       analyseLine(line, lineIndex);
     });
-    console.log('\nRESULT:\n')
+    console.log('\nRESULT:\n');
     console.log(result.toUpperCase());
   }
 });
--- instructions.js
+++ instructions.js
@@ -1,238 +1,237 @@
 const registers = [
   {
     name: 'a',
-    rel9code: '0'
+    rel9code: '0',
   },
   {
     name: 'b',
-    rel9code: '1'
+    rel9code: '1',
   },
   {
     name: 'd',
-    rel9code: '4'
+    rel9code: '4',
   },
   {
     name: 'x',
     rel9code: '5',
-    indexedCode: '000'
+    indexedCode: '000',
   },
   {
     name: 'y',
     rel9code: '6',
-    indexedCode: '01'
+    indexedCode: '01',
   },
   {
     name: 'sp',
     rel9code: '7',
-    indexedCode: '10'
+    indexedCode: '10',
   },
   {
     name: 'pc',
-    indexedCode: '11'
+    indexedCode: '11',
   },
 ];
 
 const directives = [
   {
-    name: "start"
+    name: 'start',
   },
   {
-    name: "end"
+    name: 'end',
   },
   {
-    name: "org",
+    name: 'org',
   },
   {
-    name: "equ",
+    name: 'equ',
   },
   {
-    name: "bsz",
+    name: 'bsz',
   },
   {
-    name: "zmb",
+    name: 'zmb',
   },
   {
-    name: "fill",
+    name: 'fill',
   },
   {
-    name: "fcb"
+    name: 'fcb',
   },
   {
-    name: "fcc"
+    name: 'fcc',
   },
   {
-    name: "dc.b"
-  }
+    name: 'dc.b',
+  },
 ];
 
 const addressingModes = [
   {
     //inherent
-    type: "inh",
-    testFunction: function(operandTokens) {
+    type: 'inh',
+    testFunction: function (operandTokens) {
       return !operandTokens || operandTokens.length == 0;
     },
-    parseFunction: function(operandTokens, _pc) {
+    parseFunction: function (operandTokens, _pc) {
       return '';
     },
-    length: 0
+    length: 0,
   },
   {
     //immediate8b
-    type: "imm8",
-    testFunction: function(operandTokens, _pc) {
-      if (!(/^#.*/.test(operandTokens[0]))) return false;
+    type: 'imm8',
+    testFunction: function (operandTokens, _pc) {
+      if (!/^#.*/.test(operandTokens[0])) return false;
       let numValue = parseNumber(operandTokens[0]);
       if (isNaN(numValue)) return false;
       return isInRange(numValue, 0, 2 ** 8 - 1);
     },
-    parseFunction: function(operandTokens, _pc) {
+    parseFunction: function (operandTokens, _pc) {
       return parseNumber(operandTokens[0]).toString(16).padStart(2, '0');
     },
-    length: 1
+    length: 1,
   },
   {
     //immediate16b
-    type: "imm16",
-    testFunction: function(operandTokens) {
+    type: 'imm16',
+    testFunction: function (operandTokens) {
       if (!/^#.*/.test(operandTokens[0])) return false;
       let numValue = parseNumber(operandTokens[0]);
       if (isNaN(numValue)) return false;
       return isInRange(numValue, 0, 2 ** 16 - 1);
     },
-    parseFunction: function(operandTokens, _pc) {
+    parseFunction: function (operandTokens, _pc) {
       return parseNumber(operandTokens[0]).toString(16).padStart(4, '0');
     },
-    length: 2
+    length: 2,
   },
   {
     //direct
-    type: "dir",
-    testFunction: function(operandTokens) {
+    type: 'dir',
+    testFunction: function (operandTokens) {
       if (/^#.*/.test(operandTokens[0])) return false;
       let numValue = parseNumber(operandTokens[0]);
       if (isNaN(numValue)) return false;
       return isInRange(numValue, 0, 2 ** 8 - 1);
     },
-    parseFunction: function(operandTokens, _pc) {
+    parseFunction: function (operandTokens, _pc) {
       return parseNumber(operandTokens[0]).toString(16).padStart(2, '0');
     },
-    length: 1
+    length: 1,
   },
   {
     //extended
-    type: "ext",
-    testFunction: function(operandTokens) {
+    type: 'ext',
+    testFunction: function (operandTokens) {
       if (/^#.*/.test(operandTokens[0])) return false;
       let numValue = parseNumber(operandTokens[0]);
       if (isNaN(numValue)) return false;
       return isInRange(numValue, 0, 2 ** 16 - 1);
     },
-    parseFunction: function(operandTokens, _pc) {
+    parseFunction: function (operandTokens, _pc) {
       return parseNumber(operandTokens[0]).toString(16).padStart(4, '0');
     },
-    length: 2
+    length: 2,
   },
   {
     //relative8b
-    type: "rel8",
-    testFunction: function(operandTokens, pc) {
+    type: 'rel8',
+    testFunction: function (operandTokens, pc) {
       let numValue = parseNumber(operandTokens[0]);
       if (isNaN(numValue)) return false;
       let offset = numValue - (pc + 2);
-      return isInRange(offset, -(2**7), 2**7 - 1);
+      return isInRange(offset, -(2 ** 7), 2 ** 7 - 1);
     },
-    parseFunction: function(operandTokens, pc) {
+    parseFunction: function (operandTokens, pc) {
       let numValue = parseNumber(operandTokens[0]);
       let offset = numValue - (pc + 2);
-      return parseInt(twosComplement(offset, 8), 2).toString(16).padStart(2, '0');
+      return parseInt(twosComplement(offset, 8), 2)
+        .toString(16)
+        .padStart(2, '0');
     },
-    length: 1
+    length: 1,
   },
   {
     //relative16b
-    type: "rel16",
-    testFunction: function(operandTokens, pc) {
+    type: 'rel16',
+    testFunction: function (operandTokens, pc) {
       let numValue = parseNumber(operandTokens[0]);
       if (isNaN(numValue)) return false;
       let offset = numValue - (pc + 4);
-      return isInRange(offset, -(2**15), 2**15 - 1);
+      return isInRange(offset, -(2 ** 15), 2 ** 15 - 1);
     },
-    parseFunction: function(operandTokens, pc) {
+    parseFunction: function (operandTokens, pc) {
       let numValue = parseNumber(operandTokens[0]);
       let offset = numValue - (pc + 4);
-      return parseInt(twosComplement(offset, 16), 2).toString(16).padStart(4, '0');
+      return parseInt(twosComplement(offset, 16), 2)
+        .toString(16)
+        .padStart(4, '0');
     },
-    length: 2
+    length: 2,
   },
   {
     //relative9b
-    type: "rel9",
-    testFunction: function(operandTokens, pc) {
+    type: 'rel9',
+    testFunction: function (operandTokens, pc) {
       let [reg, target] = operandTokens[0].split(',');
-      if(!findRegister(reg))
-        return false;
+      if (!findRegister(reg)) return false;
       let numValue = parseNumber(target);
       if (isNaN(numValue)) return false;
       let offset = numValue - (pc + 3);
-      return isInRange(offset, -(2**8), 2**8-1);
+      return isInRange(offset, -(2 ** 8), 2 ** 8 - 1);
     },
-    parseFunction: function(operandTokens, pc, mode) {
+    parseFunction: function (operandTokens, pc, mode) {
       let [reg, target] = operandTokens[0].split(',');
       let regInfo = findRegister(reg);
       let numValue = parseNumber(target);
       let offset = numValue - (pc + 3);
       let secondCode;
-      if(Math.sign(offset) == -1){
+      if (Math.sign(offset) == -1) {
         secondCode = (parseInt(mode.secOpcode, 16) + 1).toString(16);
         offset = parseInt(twosComplement(offset, 8), 2);
-      }
-      else
-        secondCode = mode.secOpcode;
+      } else secondCode = mode.secOpcode;
       secondCode += regInfo.rel9code;
       return `${secondCode} ${offset.toString(16)}`;
     },
-    length: 2
-  },{
+    length: 2,
+  },
+  {
     //indexed 1 - 5b
-    type: "idx1_5b",
-    testFunction: function(operandTokens, pc) {
+    type: 'idx1_5b',
+    testFunction: function (operandTokens, pc) {
       let [offset, reg] = operandTokens[0].split(',');
-      if(!findRegister(reg) || !findRegister(reg).indexedCode)
-        return false;
+      if (!findRegister(reg) || !findRegister(reg).indexedCode) return false;
       let numValue = parseNumber(offset);
       if (isNaN(numValue)) return false;
-      return isInRange(numValue, -(2**4), 2**4-1);
+      return isInRange(numValue, -(2 ** 4), 2 ** 4 - 1);
     },
-    parseFunction: function(operandTokens, pc, mode) {
+    parseFunction: function (operandTokens, pc, mode) {
       let [offset, reg] = operandTokens[0].split(',');
-      if(!findRegister(reg))
-        return false;
+      if (!findRegister(reg)) return false;
       let numValue = parseNumber(offset);
       let regInfo = findRegister(reg);
       let bx = '';
 
       bx += regInfo.indexedCode + '0';
-      bx += twosComplement(numValue, 5).padStart(5, '0')
+      bx += twosComplement(numValue, 5).padStart(5, '0');
 
       return `${bx}`;
     },
-    length: 1
-  },{
+    length: 1,
+  },
+  {
     //indexed 1 - 9b
-    type: "idx1_9b",
-    testFunction: function(operandTokens, pc) {
+    type: 'idx1_9b',
+    testFunction: function (operandTokens, pc) {
       let [offset, reg] = operandTokens[0].split(',');
-      if(!findRegister(reg) || !findRegister(reg).indexedCode)
-        return false;
+      if (!findRegister(reg) || !findRegister(reg).indexedCode) return false;
       let numValue = parseNumber(offset);
       if (isNaN(numValue)) return false;
-      return isInRange(numValue, -(2**8), 2**8-1);
+      return isInRange(numValue, -(2 ** 8), 2 ** 8 - 1);
     },
-    parseFunction: function(operandTokens, pc, mode) {
+    parseFunction: function (operandTokens, pc, mode) {
       let [offset, reg] = operandTokens[0].split(',');
-      if(!findRegister(reg))
-        return false;
+      if (!findRegister(reg)) return false;
       let numValue = parseNumber(offset);
       let regInfo = findRegister(reg);
       let bx = '';
@@ -240,341 +239,343 @@ const addressingModes = [
       bx += '111' + regInfo.indexedCode + '0' + '0';
       bx += (+(Math.sign(numValue) == -1)).toString();
       bx = parseInt(bx, 2).toString(16).padStart(2, '0');
-      offset = parseInt(twosComplement(numValue, 8), 2).toString(16).padStart(2, '0');
-      
+      offset = parseInt(twosComplement(numValue, 8), 2)
+        .toString(16)
+        .padStart(2, '0');
+
       return `${bx} ${offset}`;
     },
-    length: 2
-  },{
+    length: 2,
+  },
+  {
     //indexed 1 - 16b
-    type: "idx1_16b",
-    testFunction: function(operandTokens, pc) {
+    type: 'idx1_16b',
+    testFunction: function (operandTokens, pc) {
       let [offset, reg] = operandTokens[0].split(',');
-      if(!findRegister(reg) || !findRegister(reg).indexedCode)
-        return false;
+      if (!findRegister(reg) || !findRegister(reg).indexedCode) return false;
       let numValue = parseNumber(offset);
       if (isNaN(numValue)) return false;
-      return isInRange(numValue, -(2**15), 2**15-1);
+      return isInRange(numValue, -(2 ** 15), 2 ** 15 - 1);
     },
-    parseFunction: function(operandTokens, pc, mode) {
+    parseFunction: function (operandTokens, pc, mode) {
       let [offset, reg] = operandTokens[0].split(',');
-      if(!findRegister(reg))
-        return false;
+      if (!findRegister(reg)) return false;
       let numValue = parseNumber(offset);
       let regInfo = findRegister(reg);
       let bx = '';
 
       bx += '111' + regInfo.indexedCode + '010';
       bx = parseInt(bx, 2).toString(16).padStart(2, '0');
-      offset = parseInt(twosComplement(numValue, 16), 2).toString(16).padStart(4, '0');
+      offset = parseInt(twosComplement(numValue, 16), 2)
+        .toString(16)
+        .padStart(4, '0');
 
       return `${bx} ${offset}`;
     },
-    length: 3
+    length: 3,
   },
 ];
 
 const set = [
   {
-    name: "aba",
+    name: 'aba',
     modes: [
       {
-        type: "inh",
-        opcode: "1806",
-      }
-    ]
+        type: 'inh',
+        opcode: '1806',
+      },
+    ],
   },
   // "abx",
   // "aby",
   {
-    name: "adca",
+    name: 'adca',
     modes: [
       {
-        type: "imm8",
-        opcode: "89",
+        type: 'imm8',
+        opcode: '89',
       },
       {
-        type: "dir",
-        opcode: "99",
+        type: 'dir',
+        opcode: '99',
       },
       {
-        type: "ext",
-        opcode: "b9",
-      }
-    ]
+        type: 'ext',
+        opcode: 'b9',
+      },
+    ],
   },
   {
-    name: "adcb",
+    name: 'adcb',
     modes: [
       {
-        type: "imm8",
-        opcode: "c9",
+        type: 'imm8',
+        opcode: 'c9',
       },
       {
-        type: "dir",
-        opcode: "d9",
+        type: 'dir',
+        opcode: 'd9',
       },
       {
-        type: "ext",
-        opcode: "f9",
-      }
-    ]
+        type: 'ext',
+        opcode: 'f9',
+      },
+    ],
   },
   {
-    name: "adda",
+    name: 'adda',
     modes: [
       {
-        type: "imm8",
-        opcode: "8b",
+        type: 'imm8',
+        opcode: '8b',
       },
       {
-        type: "dir",
-        opcode: "9b",
+        type: 'dir',
+        opcode: '9b',
       },
       {
-        type: "ext",
-        opcode: "bb",
-      }
-    ]
+        type: 'ext',
+        opcode: 'bb',
+      },
+    ],
   },
   {
-    name: "addb",
+    name: 'addb',
     modes: [
       {
-        type: "imm8",
-        opcode: "cb",
+        type: 'imm8',
+        opcode: 'cb',
       },
       {
-        type: "dir",
-        opcode: "db",
+        type: 'dir',
+        opcode: 'db',
       },
       {
-        type: "ext",
-        opcode: "fb",
-      }
-    ]
+        type: 'ext',
+        opcode: 'fb',
+      },
+    ],
   },
   {
-    name: "addd",
+    name: 'addd',
     modes: [
       {
-        type: "imm16",
-        opcode: "c3",
+        type: 'imm16',
+        opcode: 'c3',
       },
       {
-        type: "dir",
-        opcode: "d3",
+        type: 'dir',
+        opcode: 'd3',
       },
       {
-        type: "ext",
-        opcode: "f3",
-      }
-    ]
+        type: 'ext',
+        opcode: 'f3',
+      },
+    ],
   },
   {
-    name: "anda",
+    name: 'anda',
     modes: [
       {
-        type: "imm8",
-        opcode: "84",
+        type: 'imm8',
+        opcode: '84',
       },
       {
-        type: "dir",
-        opcode: "94",
+        type: 'dir',
+        opcode: '94',
       },
       {
-        type: "ext",
-        opcode: "b4",
-      }
-    ]
+        type: 'ext',
+        opcode: 'b4',
+      },
+    ],
   },
   {
-    name: "andb",
+    name: 'andb',
     modes: [
       {
-        type: "imm8",
-        opcode: "c4",
+        type: 'imm8',
+        opcode: 'c4',
       },
       {
-        type: "dir",
-        opcode: "d4",
+        type: 'dir',
+        opcode: 'd4',
       },
       {
-        type: "ext",
-        opcode: "f4",
-      }
-    ]
+        type: 'ext',
+        opcode: 'f4',
+      },
+    ],
   },
   {
-    name: "andcc",
+    name: 'andcc',
     modes: [
       {
-        type: "imm8",
-        opcode: "10",
-      }
-    ]
+        type: 'imm8',
+        opcode: '10',
+      },
+    ],
   },
   {
-    name: "asl",
+    name: 'asl',
     modes: [
       {
-        type: "ext",
-        opcode: "78",
-      }
-    ]
+        type: 'ext',
+        opcode: '78',
+      },
+    ],
   },
   {
-    name: "asla",
+    name: 'asla',
     modes: [
       {
-        type: "inh",
-        opcode: "48",
-      }
-    ]
+        type: 'inh',
+        opcode: '48',
+      },
+    ],
   },
   {
-    name: "aslb",
+    name: 'aslb',
     modes: [
       {
-        type: "inh",
-        opcode: "58",
-      }
-    ]
+        type: 'inh',
+        opcode: '58',
+      },
+    ],
   },
   {
-    name: "asld",
+    name: 'asld',
     modes: [
       {
-        type: "inh",
-        opcode: "59",
-      }
-    ]
+        type: 'inh',
+        opcode: '59',
+      },
+    ],
   },
   {
-    name: "asr",
+    name: 'asr',
     modes: [
       {
-        type: "ext",
-        opcode: "77",
-      }
-    ]
+        type: 'ext',
+        opcode: '77',
+      },
+    ],
   },
   {
-    name: "bcc",
+    name: 'bcc',
     modes: [
       {
-        type: "inh",
-        opcode: "47",
-      }
-    ]
+        type: 'inh',
+        opcode: '47',
+      },
+    ],
   },
   {
-    name: "bclr",
-    modes: []
+    name: 'bclr',
+    modes: [],
   },
   {
-    name: "bcs",
-    modes: []
+    name: 'bcs',
+    modes: [],
   },
   {
-    name: "beq",
+    name: 'beq',
     modes: [
       {
-        type: "rel8",
-        opcode: "27",
-      }
-    ]
+        type: 'rel8',
+        opcode: '27',
+      },
+    ],
   },
   {
-    name: "bge",
-    modes: []
+    name: 'bge',
+    modes: [],
   },
   {
-    name: "bgnd",
-    modes: []
+    name: 'bgnd',
+    modes: [],
   },
   {
-    name: "bgt",
-    modes: []
+    name: 'bgt',
+    modes: [],
   },
   {
-    name: "bhi",
-    modes: []
+    name: 'bhi',
+    modes: [],
   },
   {
-    name: "bne",
+    name: 'bne',
     modes: [
       {
-        type: "rel8",
-        opcode: "26",
-      }
-    ]
+        type: 'rel8',
+        opcode: '26',
+      },
+    ],
   },
   {
-    name: "lbne",
+    name: 'lbne',
     modes: [
       {
-        type: "rel16",
-        opcode: "1826",
-      }
-    ]
+        type: 'rel16',
+        opcode: '1826',
+      },
+    ],
   },
   {
-    name: "jmp",
+    name: 'jmp',
     modes: [
       {
-        type: "ext",
-        opcode: "06",
-      }
-    ]
+        type: 'ext',
+        opcode: '06',
+      },
+    ],
   },
   {
-    name: "ibne",
+    name: 'ibne',
     modes: [
       {
-        type: "rel9",
-        opcode: "04",
+        type: 'rel9',
+        opcode: '04',
         secOpcode: 'a',
-      }
-    ]
+      },
+    ],
   },
 ];
 
-const findDirective = directiveName => {
-  return directives.find(directive => {
+const findDirective = (directiveName) => {
+  return directives.find((directive) => {
     return directive.name == directiveName;
   });
 };
 
-const findInstruction = instructionName => {
-  const foundInstruction = set.find(instruction => {
+const findInstruction = (instructionName) => {
+  const foundInstruction = set.find((instruction) => {
     return instruction.name == instructionName;
   });
   return foundInstruction;
 };
 
-const findAddressingMode = addressingModeType => {
-  return addressingModes.find(addressingMode => {
+const findAddressingMode = (addressingModeType) => {
+  return addressingModes.find((addressingMode) => {
     return addressingMode.type == addressingModeType;
   });
 };
 
-const findRegister = regName => {
-  return registers.find(register => {
+const findRegister = (regName) => {
+  return registers.find((register) => {
     return register.name == regName;
   });
 };
 
 function parseNumber(number) {
-  if(number == '')
-    return 0;
-  number = number.replace("#", "");
+  if (number == '') return 0;
+  number = number.replace('#', '');
   let radix;
   if (/^\$/.test(number)) radix = 16;
   else if (/^\@/.test(number)) radix = 8;
   else if (/^\%/.test(number)) radix = 2;
   else radix = 10;
-  number = number.replace(/@|%|\$/, "");
+  number = number.replace(/@|%|\$/, '');
   return parseInt(number, radix);
 }
 
@@ -587,12 +588,12 @@ function twosComplement(value, bitCount) {
 
   if (value >= 0) {
     let twosComp = value.toString(2);
-    binaryStr    = padAndChop(twosComp, '0', (bitCount || twosComp.length));
+    binaryStr = padAndChop(twosComp, '0', bitCount || twosComp.length);
   } else {
     binaryStr = (Math.pow(2, bitCount) + value).toString(2);
 
     if (Number(binaryStr) < 0) {
-      return undefined
+      return undefined;
     }
   }
 
@@ -602,7 +603,6 @@ function twosComplement(value, bitCount) {
 function padAndChop(str, padChar, length) {
   return (Array(length).fill(padChar).join('') + str).slice(length * -1);
 }
-
 
 exports.set = set;
 exports.addressingModes = addressingModes;
